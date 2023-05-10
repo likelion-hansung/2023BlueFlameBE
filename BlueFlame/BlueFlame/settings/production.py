cc642fafc091dd6f60d48adccfa2858de0d5765d
JongukYang <didwhddnr123@naver.com>
2023-05-10 20:24:33 +0900

    deploy

---
+++ BlueFlame/BlueFlame/settings/production.py
@@ -0,0 +1,27 @@
+# 배포 서버
+from .base import *
+
+# DEBUG = True
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DJANGO_APPS += []
+    
+PROJECT_APPS += []
+    
+THIRD_PARTY_APPS += []
+    
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = (BASE_DIR / 'static', )
